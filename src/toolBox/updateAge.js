be479c698b9c9423b6428b291d07507f33f935c2
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-05-01 14:53:05 -0300

    feat: return my age

---
+++ src/toolBox/updateAge.js
@@ -0,0 +1,19 @@
+const updateAge = () => {
+  const date = new Date();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const birthMonth = 6;
+  const birthDay = 26;
+
+  let age;
+  // Verifica se o aniversário já ocorreu neste ano ou não
+  if (month > birthMonth || (month === birthMonth && day >= birthDay)) {
+    age = date.getFullYear() % 100;
+  } else {
+    age = (date.getFullYear() % 100) - 1;
+  }
+
+  return age;
+};
+
+export default updateAge;
