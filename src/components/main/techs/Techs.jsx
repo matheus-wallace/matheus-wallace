931c8af91cd140bfa92a8477b547faabc9ce1083
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-05-01 23:43:48 -0300

    feat: update icons paths

--- src/components/main/techs/Techs.jsx
+++ src/components/main/techs/Techs.jsx
@@ -7,12 +7,12 @@ const Techs = () => {
     <section className="default-padding">
       <Subtitle title="Tecnologias" />
       <ul className="techs">
-        <CardMobile path={'src/assets/icons/html-mobile.svg'} alt={'html icon'} tech={'HTML'} />
-        <CardMobile path={'src/assets/icons/css-mobile.svg'} alt={'css icon'} tech={'CSS'} />
-        <CardMobile path={'src/assets/icons/javascript-mobile.svg'} alt={'javascript icon'} tech={'Javascipt'} />
-        <CardMobile path={'src/assets/icons/typescript-mobile.svg'} alt={'typescript icon'} tech={'Typescipt'} />
-        <CardMobile path={'src/assets/icons/react-mobile.svg'} alt={'react icon'} tech={'React'} />
-        <CardMobile path={'src/assets/icons/figma-mobile.svg'} alt={'figma icon'} tech={'Figma'} />
+        <CardMobile path={'./src/assets/icons/html-mobile.svg'} alt={'html icon'} tech={'HTML'} />
+        <CardMobile path={'./src/assets/icons/css-mobile.svg'} alt={'css icon'} tech={'CSS'} />
+        <CardMobile path={'./src/assets/icons/javascript-mobile.svg'} alt={'javascript icon'} tech={'Javascipt'} />
+        <CardMobile path={'./src/assets/icons/typescript-mobile.svg'} alt={'typescript icon'} tech={'Typescipt'} />
+        <CardMobile path={'./src/assets/icons/react-mobile.svg'} alt={'react icon'} tech={'React'} />
+        <CardMobile path={'./src/assets/icons/figma-mobile.svg'} alt={'figma icon'} tech={'Figma'} />
       </ul>
     </section>
   );
