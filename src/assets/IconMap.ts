1a1079fa6f7a22a85fe8020bfc1c7aa421790634
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-02-12 20:28:36 -0300

    feat: add icon map to make more easy to use with component

---
+++ src/assets/IconMap.ts
@@ -0,0 +1,10 @@
+import LinkedinIcon from "@/assets/icons/Linkedin.svg";
+
+import GithubIcon from "@/assets//icons/githubHeader.svg";
+
+const iconMap = {
+  linkedin: LinkedinIcon,
+  github: GithubIcon,
+};
+
+export default iconMap;
