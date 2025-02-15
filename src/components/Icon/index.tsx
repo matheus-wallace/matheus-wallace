09bab5ea9a830b3d880fdfc30b67dbd5d425e97e
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-02-15 17:36:01 -0300

    refactor: add props types to icon component

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { IconContainer, IconProps } from "./styles";
+import { IconContainer } from "./styles";
 import iconMap from "@/assets/IconMap";
 
-const Icon: React.FC<IconProps> = ({ name, ...props }) => {
-  if (name === undefined) {
-    console.warn("icon name prop cannot be undefind");
-    return;
-  }
+export interface IconProps {
+  name: keyof typeof iconMap;
+}
 
+const Icon: React.FC<IconProps> = ({ name, ...props }) => {
   const IconSrc = iconMap[name].src;
   console.log(IconSrc);
 
