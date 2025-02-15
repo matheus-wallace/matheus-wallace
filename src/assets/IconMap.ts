cffd607415a491ac5c1aecc9b77a9d76c1d20b54
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-02-15 20:39:02 -0300

    feat: add icons `menu` and `close`

--- src/assets/IconMap.ts
+++ src/assets/IconMap.ts
@@ -1,10 +1,13 @@
 import LinkedinIcon from "@/assets/icons/Linkedin.svg";
-
-import GithubIcon from "@/assets//icons/githubHeader.svg";
+import GithubIcon from "@/assets/icons/githubHeader.svg";
+import MenuHamburguer from "@/assets/icons/humburger.svg";
+import CloseMenu from "@/assets/icons/close-menu-mobile.svg";
 
 const iconMap = {
   linkedin: LinkedinIcon,
   github: GithubIcon,
+  menuHamburguer: MenuHamburguer,
+  closeMenu: CloseMenu,
 };
 
 export default iconMap;
